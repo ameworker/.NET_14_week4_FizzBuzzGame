63a1026b78989f73a662c6285a3d18809ccb2ede
Agnieszka Malinska <ameworker@gmail.com>
2024-06-04 10:52:07 +0200

    Upload Files to GItHub Repo

---
+++ 13_week_4_FizzBuzz_Game/FizzBuzz.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13_week_4_FizzBuzz_Game
+{
+
+    internal class FizzBuzz
+    {
+
+        public string DescribeNumber(int number)
+        {
+            if (number % 3 == 0 && number % 5 != 0)
+            {
+                Console.WriteLine("Fizz");
+                return "Fizz";
+            }
+            else if (number % 3 != 0 && number % 5 == 0)
+            {
+                Console.WriteLine("Buzz");
+                return "Buzz";
+            }
+            else if (number % 3 == 0 && number % 5 == 0)
+            {
+                Console.WriteLine("FizzBuzz");
+                return "FizzBuzz";
+            }
+            else
+            {
+                Console.WriteLine(number);
+                return number.ToString();
+            }
+        }
+    }
+}
+
---
+++ 13_week_4_FizzBuzz_Game/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13_week_4_FizzBuzz_Game
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Gra FizzBuzz 1.0");
+            Console.WriteLine();
+            Console.WriteLine("Podaj liczbę");
+            bool continueFlag = true;
+            do
+            {
+                try
+                {
+                    if (!int.TryParse(Console.ReadLine(), out int userNumber))
+                    {
+                        throw new Exception("To nie jest liczba, spróbuj ponownie");
+                    }
+
+                    FizzBuzz fzCheck = new FizzBuzz();
+                    fzCheck.DescribeNumber(userNumber);
+
+                    Console.WriteLine();
+                    Console.WriteLine("Chcesz spróbować ponownie? T/N");
+                    string decision = Console.ReadLine();
+                    if (decision.ToUpper() == "N")
+                    {
+                        continueFlag = false;
+                        Environment.Exit(0);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Podaj liczbę");
+                    }
+
+                }
+                catch (Exception ex)
+                {
+
+                    Console.WriteLine(ex.Message);
+                }
+
+            } while (continueFlag == true);
+
+            Console.ReadLine();
+        }
+    }
+}
+
